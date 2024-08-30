7201bc8fcb40c2b6ad28f52abcd80cf604fb18b8
KEYLA CONDORI <keylis999@gmail.com>
2024-08-31 00:28:35 +0200

    Página about

--- views/site/about.php
+++ views/site/about.php
@@ -1,18 +1,93 @@
 <?php
 
-/** @var yii\web\View $this */
-
-use yii\helpers\Html;
-
-$this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
+use yii\widgets\ListView;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="container quienes-somos">
+    <div class="title-somos d-flex justify-content-center flex-column align-items-center">
+        <h2 class="mb-4 mt-5">¿Quiénes somos?</h2>
+        <p>Somos Danza CFC , una escuela de danza cristiana dedicada a glorificar a Dios a través del arte del baile.
+            Fundada con la pasión de integrar la fe y la expresión artística, nuestra institución se centra en proporcionar
+            una formación de danza que no solo busca la excelencia técnica, sino que también fomenta el crecimiento espiritual
+            y el desarrollo del carácter cristiano.<br>
+            En Danza CFC, creemos que cada movimiento es una oportunidad para adorar y honrar a Dios. Nuestros programas están
+            diseñados para todos los niveles y edades, desde principiantes hasta avanzados, ofreciendo una variedad de estilos
+            de danza que incluyen danza con pandero, danza hebrea y profética, siempre con un enfoque en principios y valores cristianos.<br>
+            Nuestro equipo está compuesto por instructores altamente calificados y apasionados, quienes no solo son expertos en su campo,
+            sino también mentores comprometidos con el bienestar y crecimiento integral de nuestros estudiantes. Nos esforzamos por
+            crear un ambiente de apoyo y amor, donde cada estudiante se sienta valorado y motivado a alcanzar su máximo potencial.<br>
+            En Danza CFC, nuestra comunidad es una extensión de nuestra fe. Organizamos eventos, presentaciones y proyectos de servicio
+            que permiten a nuestros estudiantes utilizar sus talentos para impactar positivamente a la comunidad y compartir el mensaje
+            de esperanza y amor de Cristo.<br>
+            Únete a nosotros en Danza CFC, donde la danza y la fe se unen para crear algo verdaderamente hermoso y
+            significativo.
+        </p>
+    </div>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+    <!-- VISIÓN Y MISIÓN POSIBLE -->
+    <div class="row">
+        <div class="col-6 d-flex align-items-center flex-column">
+            <h3>Visión</h3>
+            <p>"Ser una comunidad de fe donde la danza se convierte en una expresión viva de la adoración a Dios, inspirando y
+                transformando vidas a través del arte y el compromiso cristiano. Aspiramos a ser un faro de luz y esperanza,
+                promoviendo la excelencia en la danza mientras cultivamos valores de amor, fe y servicio en cada estudiante."
+            </p>
+        </div>
+        <div class="col-6 d-flex align-items-center flex-column">
+            <h3>Misión</h3>
+            <p>"Nuestra misión es glorificar a Dios y edificar Su reino a través de la enseñanza de la danza. Nos dedicamos
+                a proporcionar una formación integral que combina la excelencia técnica con el desarrollo espiritual.
+                Fomentamos un ambiente donde los estudiantes pueden crecer en su fe, descubrir sus talentos dados por Dios
+                y usar sus habilidades para impactar positivamente a la comunidad y el mundo. Nos comprometemos a inculcar
+                valores cristianos, promoviendo el respeto, la humildad y el amor en todas nuestras actividades."
+            </p>
+        </div>
+    </div>
 
-    <code><?= __FILE__ ?></code>
+    <div class="tarjetas-pasos d-flex justify-content-center mt-5">
+        <div class="card card-tarjeta-pasos" style="width: 18rem;">
+            <div class="contain-image">
+                <img class="card-img-top" src="../../web/imagenes/Directora.jpg" alt="Card image cap">
+            </div>
+            <div class="card-body d-flex justify-content-center flex-column align-items-center">
+                <h5 class="card-title text-center"> Directora <br>Fidelina Hamilton</h5>
+                <p class="card-text text-center">
+                     
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6">
+            <!-- TARJETA 1 -->
+            <div class="tarjetas-pasos d-flex justify-content-center mt-5">
+                <div class="card card-tarjeta-pasos" style="width: 18rem;">
+                    <div class="contain-image">
+                        <img class="card-img-top" src="../../web/imagenes/Lider.jpg" alt="Card image cap">
+                    </div>
+                    <div class="card-body d-flex justify-content-center flex-column align-items-center">
+                        <h5 class="card-title text-center">Líder <br>Helena Canty</h5>
+                        <p class="card-text text-center">
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <!-- TARJETA 2 -->
+            <div class="tarjetas-pasos d-flex justify-content-center mt-5">
+                <div class="card card-tarjeta-pasos" style="width: 18rem;">
+                    <div class="contain-image">
+                        <img class="card-img-top" src="../../web/imagenes/Secretaria.jpg" alt="Card image cap">
+                    </div>
+                    <div class="card-body d-flex justify-content-center flex-column align-items-center">
+                        <h5 class="card-title text-center">Secretaria<br> Keyla Mccue</h5>
+                        <p class="card-text text-center">
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
